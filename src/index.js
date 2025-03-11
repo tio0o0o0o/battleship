4bdf490e56c0a8d6a92266f5cc32dfb992ffc094
Taiyo <taiyoperson@gmail.com>
2025-03-12 10:56:24 +1300

    style: template

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-const GameController = require("./controllers/game-controller.js");
-const Player = require("./models/player.js");
+// const GameController = require("./controllers/game-controller.js");
+// const Player = require("./models/player.js");
 
-const human = new Player("PLAYER", "human", 10);
-const computer = new Player("COMPUTER", "computer", 10);
+// const human = new Player("PLAYER", "human", 10);
+// const computer = new Player("COMPUTER", "computer", 10);
 
-human.board.placeShip(3, 2, 4, "horizontal");
-human.board.placeShip(7, 5, 3, "vertical");
-human.board.placeShip(1, 8, 5, "horizontal");
-human.board.placeShip(9, 0, 2, "vertical");
+// human.board.placeShip(3, 2, 4, "horizontal");
+// human.board.placeShip(7, 5, 3, "vertical");
+// human.board.placeShip(1, 8, 5, "horizontal");
+// human.board.placeShip(9, 0, 2, "vertical");
 
-computer.board.placeShip(0, 4, 5, "vertical");
-computer.board.placeShip(5, 3, 4, "horizontal");
-computer.board.placeShip(8, 7, 3, "vertical");
-computer.board.placeShip(2, 6, 2, "horizontal");
+// computer.board.placeShip(0, 4, 5, "vertical");
+// computer.board.placeShip(5, 3, 4, "horizontal");
+// computer.board.placeShip(8, 7, 3, "vertical");
+// computer.board.placeShip(2, 6, 2, "horizontal");
 
-const gameController = new GameController("player1", human, computer);
+// const gameController = new GameController("player1", human, computer);
 
-gameController.playGame();
+// gameController.playGame();
+
+require("./assets/styles/style.css");

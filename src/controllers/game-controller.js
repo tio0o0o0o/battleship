cf5d51afce928eed36af713c48380f9038317839
Taiyo <taiyoperson@gmail.com>
2025-03-11 23:21:46 +1300

    feat: create humanTurn for GameController

--- src/controllers/game-controller.js
+++ src/controllers/game-controller.js
@@ -9,6 +9,48 @@ class GameController {
     this.player1 = player1;
     this.player2 = player2;
   }
+
+  async humanTurn() {
+    let input;
+    let attackResult;
+    while (true) {
+      console.clear();
+      GameView.printBoards(this.player1, this.player2);
+      input = await CLI.promptAsync("Enter target [x, y]: ");
+      if (input.match(/^\d+, \d+$/)) {
+        const [x, y] = input.split(", ");
+        attackResult = this.player2.board.receiveAttack(x, y);
+        if (attackResult === "overlap") {
+          await CLI.promptAsync("You have already attacked there! ");
+        } else if (attackResult === "outOfBounds") {
+          await CLI.promptAsync("Coordinates are out of bounds! ");
+        } else break;
+      } else {
+        await CLI.promptAsync("Enter valid coordinates! ");
+      }
+    }
+    console.clear();
+    GameView.printBoards(this.player1, this.player2);
+    if (attackResult.allSunk) {
+      await CLI.promptAsync("You won the game! ");
+      return "allSunk";
+    } else if (attackResult.isSunk) {
+      await CLI.promptAsync("You sunk an enemy ship! ");
+      return "sunk";
+    } else if (attackResult.result === "hit") {
+      await CLI.promptAsync("You hit a ship! ");
+      return "hit";
+    } else if (attackResult.result === "miss") {
+      await CLI.promptAsync("You missed! ");
+      return "miss";
+    }
+  }
+
+  async playGame() {
+    while (true) {
+      await this.humanTurn();
+    }
+  }
 }
 
 module.exports = GameController;
